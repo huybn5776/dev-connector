8585df3b404329c32e02cba328e7dfa86ab68b96
huybn5776 <>
2021-07-09 23:12:18 +0800

    feat: define return type of actionNameCreator with Template Literal Types
    https://www.typescriptlang.org/docs/handbook/2/template-literal-types.html

--- client/src/utils/action-utils.ts
+++ client/src/utils/action-utils.ts
@@ -1,14 +1,18 @@
-export function actionNameCreator(state: string): {
-  actionName: (action: string) => string;
-  asyncActionNames: (action: string) => [string, string, string];
+type ActionNameFormat<S extends string, A extends string> = [`[${S}] ${A}`, `[${S}] ${A} success`, `[${S}] ${A} fail`];
+
+export function actionNameCreator<S extends string>(
+  state: S,
+): {
+  actionName: <A extends string>(action: A) => ActionNameFormat<S, A>[0];
+  asyncActionNames: <A extends string>(action: A) => ActionNameFormat<S, A>;
 } {
   return {
-    actionName: (action: string) => `${state} ${action}`,
-    asyncActionNames: (action: string) => {
+    actionName: <A extends string>(action: A) => `[${state}] ${action}`,
+    asyncActionNames: <A extends string>(action: A) => {
       const request = `[${state}] ${action}`;
       const success = `[${state}] ${action} success`;
       const fail = `[${state}] ${action} fail`;
-      return [request, success, fail];
+      return [request, success, fail] as ActionNameFormat<S, A>;
     },
   };
 }
